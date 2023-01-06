a7d066c30d0a95c4e11d15c5432a4b14d61f3b31
ulises <ulisessahlin@hotmail.com>
2023-01-06 01:16:54 -0300

    Route

--- LaRuinaTv-Client/client/src/index.js
+++ LaRuinaTv-Client/client/src/index.js
@@ -10,25 +10,19 @@ import {
   Link,
 } from "react-router-dom";
 import Browser from './Components/Browser';
-import Novedades from './Components/Novedades';
-import Colaborar from './Components/Colaborar';
-import Lanzamientos from './Components/Lanzamientos';
-import Tienda from './Components/Tienda';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "browser",
+        element: <Browser />,
+      },
+  ]}
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Routes>
-        <Route exact path='/browser' element={<Browser/>}/>
-        <Route exact path='/novedades' element={<Novedadses/>}/>
-        <Route exact path='/colaborar' element={<Colaborar/>}/>
-        <Route exact path='/lanzamientos' element={<Lanzamientos/>}/>
-        <Route exact path='/tienda' element={<Tienda/>}/>
-      </Routes>
-    </BrowserRouter>
-    </React.StrictMode>
-  
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
