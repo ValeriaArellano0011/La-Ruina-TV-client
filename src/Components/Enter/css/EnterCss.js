4658bd74f4df11216facb70cdac1f54a36230e78
GwerhBard <lalo@perronegro.cl>
2023-01-07 08:01:17 -0300

    Checked

--- src/Components/Enter/css/EnterCss.js
+++ src/Components/Enter/css/EnterCss.js
@@ -2,6 +2,6 @@ export const EnterCss = () => {
     return(
     document.querySelector('body').style.backgroundColor= `#000000`,
     document.querySelector('body').style.backgroundPosition="center",
-    document.querySelector('body').style.overflow= 'hidden',
+    document.querySelector('body').style.overflowX= 'hidden',
     document.querySelector('body').style.height= '100vh'
 )}
