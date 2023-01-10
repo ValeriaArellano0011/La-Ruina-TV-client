a00802d6d46e74d6ccda349c20b5c93dbcf5176a
GwerhBard <lalo@perronegro.cl>
2023-01-10 20:29:11 -0300

    fvck!ng thrvsh

--- src/Components/Media/Player.jsx
+++ src/Components/Media/Player.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const Player = (props) => {
   const {id}= props
-
   return (
     <div className='playerCont'>
       <ul className='playUl'><iframe 
         width="600px" 
         height="350px" 
-        src={`https://www.youtube.com/embed/${id}`}
-        title="YouTube video player" 
+        src={`https://www.youtube.com/embed/${id}?start=0;rel=0&amp`}
+        Player
         frameborder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         allowfullscreen='true'>
