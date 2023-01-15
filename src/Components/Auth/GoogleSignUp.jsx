20cbbc82bb032de9bd9d0d891b89570bc4438951
ValeriaArellano0011 <valeria.arellano0011@gmail.com>
2023-01-15 01:11:04 -0300

    fixed issues and warning to deploy

--- src/Components/Auth/GoogleSignUp.jsx
+++ src/Components/Auth/GoogleSignUp.jsx
@@ -1,30 +1,18 @@
 import s from "./css/GoogleAuth.module.css";
 import googleIcon from "../../design/google-icon.png";
-import { useDispatch } from "react-redux";
-import { googleAuth } from "../../middlewares/redux/actions";
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import axios from "axios";
+
 
 export default function GoogleSignUp() {
-  const dispatch = useDispatch();
   const history = useHistory;
-
-  const handleOnClick = async () => {
-    const response = await axios.get(
-      `https://la-ruina-api.fly.dev/auth/google`
-    );
-    console.log(response);
-    // const token = response.data;
-    // localStorage.setItem('token', token);
-  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       history.push("/browser");
     }
-  }, []);
+  }, [history]);
 
   return (
     <div>
