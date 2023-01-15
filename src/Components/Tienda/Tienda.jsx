f43bddff55dd98ee481ba9904165dc0a69b925fa
GwerhBard <lalo@perronegro.cl>
2023-01-15 08:09:13 -0300

    css

--- src/Components/Tienda/Tienda.jsx
+++ src/Components/Tienda/Tienda.jsx
@@ -20,7 +20,6 @@ export const Tienda = () => {
       <div className={s.tiendaBg}>
         <div className={s.tiendaSliderCont}>
           <h1 className={s.tiendaTituloSlider}>Bienvenidos a la tienda oficial de La Ruina Records</h1>
-          {/* <img classname={s.tiendaSlider} src={bannerTienda} alt="" /> */}
           <div className='bgNav'></div>
         </div>
         <ul className={s.ulTitle}>
