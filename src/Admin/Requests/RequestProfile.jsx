c45bdf78aafa5df917db8aff1fb36549e1f26de3
GwerhBard <lalo@perronegro.cl>
2023-01-15 10:22:56 -0300

    fvckin thrvsh!

--- src/Admin/Requests/RequestProfile.jsx
+++ src/Admin/Requests/RequestProfile.jsx
@@ -1,78 +1,47 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { resetOption } from '../../middlewares/redux/actions'
 import s from './css/OptionsProfile.module.css'
 import likeIcon from '../../design/like-icon.png'
 import listIcon from '../../design/lista-icon.png'
 import logoutIcon from '../../design/logout-icon.png'
+import adminIcon from '../../design/admin-icon.png'
 
 const RequestProfile = () => {
     const dispatch = useDispatch()
     const history = useHistory()
+    const rolUser = useSelector(state=>state.rolUser)
     return (
         <div className={s.divProfileMenu}>
             <ul
-                className={s.ulProfileMenu}
-                onClick={() => {
-                    return (
-                    dispatch(resetOption()),
-                    document.querySelector(`.bodyApp`).style.transform = 'translateX(0)',
-                    document.querySelector(`.navCont`).style.transitionDuration = '.2s',
-                    document.querySelector(`.bodyApp`).style.transitionDuration = '2s',
-                    document.querySelector(`.navCont`).style.width = '100vw',
-                    document.querySelector(`.navMenu`).style.display = 'flex',
-                    document.querySelector(`.browserBody`).style.height = 'auto',
-                    document.querySelector(`.browserBody`).style.overflowY = 'scroll',
-                    document.querySelector(`.visor`).style.transform = 'translateX(0)',
-                    document.querySelector('#slideCanvasCont').style.overflowY = "scroll"
-                    )
-                }}>
+                className={s.ulProfileMenu}>
                 <ul className={s.ulRequestProfile}>
-                <li
-                onClick={()=>{
-                    return(
-                    dispatch(resetOption()),
-                    document.querySelector(`.bodyApp`).style.transform='translateX(0)',
-                    document.querySelector(`.navCont`).style.transitionDuration='.2s',
-                    document.querySelector(`.bodyApp`).style.transitionDuration='2s',
-                    document.querySelector(`.navCont`).style.width='100vw',
-                    document.querySelector(`.navMenu`).style.display='flex',
-                    document.querySelector(`.browserBody`).style.height='auto',
-                    document.querySelector(`.browserBody`).style.overflowY='scroll',
-                    document.querySelector(`.visor`).style.transform='translateX(0)',
-                    document.querySelector('#slideCanvasCont').style.overflowY="scroll"
-                    )}}>
-                <img src={likeIcon} className={s.likeIcon} alt="favoritos" /></li>
-                <li
-                onClick={()=>{
-                    return(
-                    dispatch(resetOption()),
-                    document.querySelector(`.bodyApp`).style.transform='translateX(0)',
-                    document.querySelector(`.navCont`).style.transitionDuration='.2s',
-                    document.querySelector(`.bodyApp`).style.transitionDuration='2s',
-                    document.querySelector(`.navCont`).style.width='100vw',
-                    document.querySelector(`.navMenu`).style.display='flex',
-                    document.querySelector(`.browserBody`).style.height='auto',
-                    document.querySelector(`.browserBody`).style.overflowY='scroll',
-                    document.querySelector(`.visor`).style.transform='translateX(0)',
-                    document.querySelector('#slideCanvasCont').style.overflowY="scroll"
-                    )}}>
-                <img src={listIcon} className={s.listIcon} alt="lista" /></li>
-                <li>          
-                <img 
-                    src={logoutIcon} 
-                    className={s.logoutIcon} 
-                    alt="salir" 
-                    onClick={()=>{
-                        return (
-                        localStorage.removeItem('auth'), 
-                        history.push('/browser'),
-                        window.location.reload()
-                        )
-                    }}/></li>
-
-                    
+                    <li>
+                        <img src={likeIcon} className={s.likeIcon} alt="favoritos" />
+                    </li>
+                    <li>
+                        <img src={listIcon} className={s.listIcon} alt="lista" />
+                    </li>
+                    {   
+                        rolUser === 'admin' ?
+                        <li>
+                            <img src={adminIcon} className={s.adminIcon} alt="lista" />
+                        </li> : null 
+                    }
+                    <li>
+                        <img 
+                            src={logoutIcon} 
+                            className={s.logoutIcon} 
+                            alt="salir" 
+                            onClick={()=>{
+                                return (
+                                localStorage.removeItem('auth'), 
+                                history.push('/browser'),
+                                window.location.reload()
+                                )
+                            }}/>
+                    </li>
                 </ul>
             </ul>
 
