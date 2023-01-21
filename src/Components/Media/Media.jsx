109d9f78aa601c57afe1b10604e41bfff7383df4
Vale Arellano <valeria.arellano0011@gmail.com>
2023-01-20 23:06:57 -0300

    added edit and delete media on front

--- src/Components/Media/Media.jsx
+++ src/Components/Media/Media.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import playIconn from "../../design/ruinatv-icon-play-n.png";
 import { Link } from "react-router-dom";
+import editIcon from '../../design/edit-icon.png';
+import deleteIcon from '../../design/delete-icon.png';
+import { useDispatch, useSelector } from "react-redux";
+import s from './css/Media.module.css';
+import { getDeleteMedia, getEditMedia } from "../../middlewares/redux/actions";
 
 const Media = ({ cardList, style, keyID }) => {
+  const rolUser = useSelector(state=>state.rolUser)
+  const dispatch = useDispatch()
   return (
     <div className={style.sliderItems}>
       <ul
@@ -35,6 +42,18 @@ const Media = ({ cardList, style, keyID }) => {
                         }}
                       >
                         {/* <img src={e.icon} alt="logo" className={style.logoItem} /> */}
+                        {rolUser==='admin'?(
+                          <ul className={s.adminRequest}>
+                            <li className={s.adminBtn}>
+                              <img src={editIcon} onClick={()=>{return dispatch(getEditMedia(e.id))}} className={s.editImg} alt='edit' width='15px' />
+                            </li>
+                            <li className={s.adminBtn}>
+                              <img src={deleteIcon} onClick={()=>{return dispatch(getDeleteMedia(e.id))}} className={s.deleteImg} alt='delete' width='15px' />
+                            </li>
+                          </ul>
+                        )
+                        : null
+                        }
                       </button>
                     </Link>
                     <Link to={``} className={style.link}>
