dffadea8bac8184f4c79071271b4e00f15558844
GwerhBard <lalo@perronegro.cl>
2023-01-20 21:14:48 -0300

    FVCK!NG THRVSH

--- src/Admin/Requests/Requests.jsx
+++ src/Admin/Requests/Requests.jsx
@@ -8,7 +8,7 @@ const Requests = () => {
   const dispatch = useDispatch()
   return (
     <div>
-      <Link to='/product/create'> 
+      {/* <Link to='/product/create'> 
         <button 
           className={s.crearProducto}
           onClick={()=>{
@@ -26,7 +26,7 @@ const Requests = () => {
               )}}>
         Crear Producto
         </button>
-      </Link>
+      </Link> */}
       <Link to='/media/create'>
         <button 
           className={s.crearPost}
@@ -45,6 +45,26 @@ const Requests = () => {
             document.querySelector('#slideCanvasCont').style.overflowY="scroll"
             )}}>
         Crear Media
+        </button>
+      </Link>
+      <Link to='/media/edit'>
+        <button 
+          className={s.crearPost}
+          onClick={()=>{
+            return(
+            dispatch(resetOption()),
+            document.querySelector(`.bodyApp`).style.transform='translateX(0)',
+            document.querySelector(`.navCont`).style.transitionDuration='.2s',
+            document.querySelector(`.bodyApp`).style.transitionDuration='2s',
+            document.querySelector(`.navCont`).style.width='0vw',
+            document.querySelector(`.navMenu`).style.display='flex',
+            document.querySelector(`.navCont`).style.width='100vw',
+            document.querySelector(`.browserBody`).style.height='auto',
+            document.querySelector(`.browserBody`).style.overflowY='scroll',
+            document.querySelector(`.visor`).style.transform='translateX(0)',
+            document.querySelector('#slideCanvasCont').style.overflowY="scroll"
+            )}}>
+        Modificar Media
         </button>
       </Link>
     </div>
