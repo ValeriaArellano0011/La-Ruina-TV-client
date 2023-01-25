61829567dc71b82ae05b1e0c9051e3b32304022e
Vale Arellano <valeria.arellano0011@gmail.com>
2023-01-25 16:49:46 -0300

    asdasf

--- src/Components/MercadopagoPlan/PlanMP.jsx
+++ src/Components/MercadopagoPlan/PlanMP.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { URL_API } from '../../middlewares/misc/config';
 
 const PlanMP = () => {
 
     const userId = JSON.parse(localStorage.getItem('auth')).userId
-    const {preapproval_id} = useParams()
-    
+
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const prea = params.get('preapproval_id');
+
     useEffect(() => {
-        console.log('el preeeeeeeea', preapproval_id)
+        console.log('el preeeeeeeea', prea)
         axios.post(`${URL_API}/mercadopago/plan`, {userId, preapproval_id})
         .then(res=> console.log(res))
         .catch(err => console.log(err))
