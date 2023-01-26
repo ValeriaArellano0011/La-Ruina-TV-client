04be86954ef3ef90ffa6fb221fbbb3651a8b0ef6
Vale Arellano <valeria.arellano0011@gmail.com>
2023-01-26 00:03:12 -0300

    dajsd

--- src/Components/MercadopagoPlan/PlanMP.jsx
+++ src/Components/MercadopagoPlan/PlanMP.jsx
@@ -12,10 +12,12 @@ const PlanMP = () => {
     const preapproval_id = params.get('preapproval_id');
 
     useEffect(() => {
+        if(preapproval_id){
         console.log('el preeeeeeeea', preapproval_id)
         axios.post(`${URL_API}/mercadopago/plan`, {userId, preapproval_id}, {withCredentials: true})
         .then(res=> console.log(res))
         .catch(err => console.log(err))
+        }
     }, [])
     return(
         <div>MPPPPPPPPPPPPPPPPPP</div>
