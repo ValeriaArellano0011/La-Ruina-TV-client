b7bb77f5e0789cc71851d868110f1e1b75b2323c
Lalo Freak <lalo.gwerh@gmail.com>
2023-02-03 01:34:53 -0300

    thrash

--- src/Admin/Profile/Profile.jsx
+++ src/Admin/Profile/Profile.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import OptionsProfile from './OptionsProfile'
+import React, {useState, useEffect} from 'react';
+import RequestProfile from '../Requests/RequestProfile';
+import { useSelector } from 'react-redux';
+import s from './css/Profile.module.css';
+import userIcon from '../../design/user-icon.png';
+import { DeleteAccount } from './DeleteAccount';
 
 const Profile = () => {
+  const [ userAlias, setUserAlias ] = useState('')
+  const [ userPicGoogle, setUserPicGoogle ] = useState('')
+  const currentUser = useSelector(state=>state.currentUser)
+  const auth = localStorage.getItem('auth');
+  const user = auth ? JSON.parse(auth) : null;
+
+  useEffect(() => {
+    if (user) {
+      setUserAlias(user.userAlias)
+      if(user.googlePic){
+        setUserPicGoogle(user.googlePic)
+        console.log(user.googlePic)
+      }
+    }
+  }, [user]);
+
   return (
     <div >
-        <OptionsProfile/>
+      <div className='dashControlCont'>
+        <div className='divProfile'>
+        <div className='navFixed' ></div>
+          <div className={s.profileCont}>
+            <ul className={s.ulListProfile}>
+              <li className={s.liProfile1}>Perfil de</li>
+              <li className={s.liProfile2}><h1>{userAlias ? userAlias : currentUser? currentUser.userAlias : "Usuario"}</h1></li>
+              <li><img className={s.userIcon} referrerPolicy="no-referrer" src={userPicGoogle ? userPicGoogle : userIcon} height='100px' alt="foto de perfil" /></li>
+              <li className={s.liProfile3}>{currentUser? currentUser.subs : 'Free Plan'}</li>
+            </ul>
+          </div>
+
+          {/* aqui */}
+          <button className={s.btnDeleteAccount} onClick={()=>{return document.querySelector('#deleteAccount').style.display='flex'}} >Eliminar cuenta</button>
+          <div className={s.deleteAccount} id={'deleteAccount'}>
+            <DeleteAccount/>
+          </div>
+
+          {/* aqui */}
+
+          <ul>
+            <li>
+              <RequestProfile/>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
