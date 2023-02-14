621a8000065b0362a3591ccaed59839d9fcd923e
Lalo Freak <lalo.gwerh@gmail.com>
2023-02-14 02:32:45 -0300

    thrash

--- src/Components/Auth/Enter.jsx
+++ src/Components/Auth/Enter.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-// import GoogleSignUp from '../Auth/GoogleSignUp'
+import GoogleSignUp from '../Auth/GoogleSignUp'
 import style from './css/Enter.module.css'
 import { EnterCss } from './js/EnterCss'
-import LogIn from './LogIn'
+// import LogIn from './LogIn'
 
 const Enter = () => {
   EnterCss()
@@ -13,8 +13,8 @@ const Enter = () => {
         <div className={style.welcomeCont} id='welcomeCont'>
         <h1>ENTRÁ BOBO, VENÍ PARA ACÁ</h1>
         <h4>Debes ingresar con una cuenta de Google</h4>
-        <LogIn/>
-        {/* <GoogleSignUp/> */}
+        {/* <LogIn/> */}
+        <GoogleSignUp/>
       </div>
      </div>
     </div>
