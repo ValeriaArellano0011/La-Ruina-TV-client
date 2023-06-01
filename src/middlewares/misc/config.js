e2cf6567ac310bdff1036c8dbd1b1eb57c298c49
Lalo Freak <lalofreak.dev@gmail.com>
2023-06-01 03:00:30 -0400

    fixing Vale's damage

--- src/middlewares/misc/config.js
+++ src/middlewares/misc/config.js
@@ -1,3 +1,1 @@
- export const URL_API = 'https://terminalkiller.fly.dev' //---->esta es desarrollo
-// export const URL_API = 'https://laruinatv-back2.fly.dev' //---->esta es estable
-// export const URL_API = 'https://localhost:8080'
+ export const URL_API = 'https://laruina-tv.fly.dev'
