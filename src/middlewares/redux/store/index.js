3906f0073c3b40c8549fab04d41261c9889043d8
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-07 09:09:42 -0300

    environment variables rectified

--- src/middlewares/redux/store/index.js
+++ src/middlewares/redux/store/index.js
@@ -1,28 +1,27 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducer';
 import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
+import { DEVELOPMENT } from '../../misc';
+import { environment } from '../../../environment';
 
+
+var store = {};
 const composeEnhancer = window.REDUX_DEVTOOLS_EXTENSIONS_COMPOSE || compose;
-const store = createStore(
-    rootReducer,
-    composeEnhancer(applyMiddleware(thunkMiddleware))
-)
 
-export default store
+if(environment === DEVELOPMENT){
+    store = createStore(
+        rootReducer,
+        compose(
+            applyMiddleware(thunk),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    )
+} else {
+    store = createStore(
+        rootReducer,
+        composeEnhancer(applyMiddleware(thunkMiddleware))
+    )
+}
 
-
-/* TEST LALO */
-
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import rootReducer from '../reducer';
-// import thunk from 'redux-thunk';
-
-// const store = createStore(
-//     rootReducer,
-//     compose(
-//         applyMiddleware(thunk),
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-// )
-
-// export default store
+export default store;
