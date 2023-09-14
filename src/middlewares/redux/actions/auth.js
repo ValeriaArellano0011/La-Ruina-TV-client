e42a3d7b69fa72ccaf7701ebe2d78b389a18b33f
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-14 20:49:02 -0300

    account update system

--- src/middlewares/redux/actions/auth.js
+++ src/middlewares/redux/actions/auth.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { URL_API } from "../../config";
 import { CURRENT_USER } from "../../misc";
 import { options } from "../../helpers";
+import { $d } from "../../../functions";
 
 export function auth(history) {
   return async function (dispatch) {
@@ -26,6 +27,14 @@ export function loginInner(email, password, history) {
       .then(res => {
         localStorage.setItem('userToken', res.data.token);
         res.data.logged && history.push(`/auth?token=${res.data.token}`);
+        $d(`.bodyApp`).style.transform='translateX(0)';
+        $d(`.navCont`).style.transitionDuration='.2s';
+        $d(`.bodyApp`).style.transitionDuration='.2s';
+        $d(`.navCont`).style.width='100vw';
+        $d(`.browserBody`).style.height='auto';
+        $d(`.browserBody`).style.overflowY='scroll';
+        $d(`.visor`).style.transform='translateX(0)';
+        $d('#slideCanvasCont').style.overflowY="scroll";
       })
       .catch((e) => {
         console.error(e);
